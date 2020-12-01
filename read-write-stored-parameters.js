296545a3c263c1a780e634801ce1bd4cb519e68d
Ryan Stanley <stanleyrya@gmail.com>
2020-11-30 22:52:33 -0500

    Cleanup

--- read-write-stored-parameters.js
+++ read-write-stored-parameters.js
@@ -1,35 +1,35 @@
 const params = {"apiKey": "testtest"};
 
+function getCurrentDir() {
+    const fm = FileManager.local();
+    const thisScriptPath = module.filename;
+    return thisScriptPath.replace(fm.fileName(thisScriptPath, true), '');
+}
+
 /**
  * Attempts to load the file ./storage/name.json
  * Returns null if it cannot be loaded.
  */
 function loadStoredParameters(name) {
-    let fm = FileManager.local()
-
-    const thisScriptPath = module.filename
-    const storageDir = thisScriptPath.replace(fm.fileName(thisScriptPath, true), '') + "storage"
-    const parameterFile = name + ".json";
-    const parameterPath = storageDir + "/" + parameterFile;
+    const fm = FileManager.local();
+    const storageDir = getCurrentDir() + "storage";
+    const parameterPath = storageDir + "/" + name + ".json";
 
     if (!fm.fileExists(storageDir)) {
         console.log("Storage folder does not exist!");
         return null;
-    }
-
-    if (fm.fileExists(storageDir) && !fm.isDirectory(storageDir)) {
+    } else if (!fm.isDirectory(storageDir)) {
         console.log("Storage folder exists but is not a directory!");
         return null;
-    }
-
-    if (!fm.fileExists(parameterPath)) {
+    } else if (!fm.fileExists(parameterPath)) {
         console.log("Parameter file does not exist!");
         return null;
+    } else if (fm.isDirectory(parameterPath)) {
+        console.log("Parameter file is a directory!");
+        return null;
     }
-    
-    const parameterString = fm.readString(parameterPath);
 
-    const parameterJSON = JSON.parse(parameterString)
+    const parameterJSON = JSON.parse(fm.readString(parameterPath));
     if (parameterJSON !== null) {
         return parameterJSON;
     } else {
@@ -40,34 +40,26 @@ function loadStoredParameters(name) {
 
 /**
  * Attempts to write the file ./storage/name.json
- * Returns false if it cannot be written.
  */
 function writeStoredParameters(name, params) {
-    let fm = FileManager.local()
-
-    const thisScriptPath = module.filename
-    const storageDir = thisScriptPath.replace(fm.fileName(thisScriptPath, true), '') + "storage"
-    const parameterFile = name + ".json";
-    const parameterPath = storageDir + "/" + parameterFile;
+    const fm = FileManager.local();
+    const storageDir = getCurrentDir() + "storage";
+    const parameterPath = storageDir + "/" + name + ".json";
 
     if (!fm.fileExists(storageDir)) {
         console.log("Storage folder does not exist! Creating now.");
         fm.createDirectory(storageDir);
     } else if (!fm.isDirectory(storageDir)) {
-        console.error("Storage folder exists but is not a directory!");
-        return false;
+        throw("Storage folder exists but is not a directory!");
     }
 
     if (fm.fileExists(parameterPath) && fm.isDirectory(parameterPath)) {
-        console.error("Parameter file is a directory, please delete!");
-        return false;
+        throw("Parameter file is a directory, please delete!");
     }
 
-    fm.writeString(parameterPath, JSON.stringify(params))
-
-    return true;
+    fm.writeString(parameterPath, JSON.stringify(params));
 }
 
-console.log(loadStoredParameters("test"))
-console.log(writeStoredParameters("test", params))
-console.log(loadStoredParameters("test"))
+console.log(loadStoredParameters("test"));
+writeStoredParameters("test", params);
+console.log(loadStoredParameters("test"));
