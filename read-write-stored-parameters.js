dc5e72b82de1de9cddffbdbb2bda4597dad542b6
Ryan Stanley <stanleyrya@gmail.com>
2020-12-08 02:23:21 +0000

    Moving over new JSON file writer class

--- read-write-stored-parameters.js
+++ read-write-stored-parameters.js
@@ -2,79 +2,75 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: cyan; icon-glyph: file-signature;
 
+/**
+ * Class that can read and write JSON objects using the file system.
+ */
+class JSONFileManager {
+
+    /**
+     * Attempts to write the jsonObject to the relative path.
+     */
+    write(relativePath, jsonObject) {
+        const fm = this.getFileManager();
+        const jsonPath = this.getCurrentDir() + relativePath;
+
+        const splitRelativePath = relativePath.split("/");
+        if (splitRelativePath > 1) {
+            const fileName = splitRelativePath[splitRelativePath.length - 1];
+            const jsonDirectory = jsonPath.replace("/" + fileName, "");
+            fm.createDirectory(jsonDirectory, true);
+        }
+
+        if (fm.fileExists(jsonPath) && fm.isDirectory(jsonPath)) {
+            throw ("JSON file is a directory, please delete!");
+        }
+
+        fm.writeString(jsonPath, JSON.stringify(jsonObject));
+    }
+
+    /**
+     * Attempts to load JSON stored at the relative path.
+     */
+    read(relativePath) {
+        const fm = this.getFileManager();
+        const jsonPath = this.getCurrentDir() + relativePath;
+
+        if (!fm.fileExists(jsonPath)) {
+            throw ("JSON file does not exist! Could not load: " + jsonPath);
+        } else if (fm.isDirectory(jsonPath)) {
+            throw ("JSON file is a directory! Could not load: " + jsonPath);
+        }
+
+        // Doesn't fail with local filesystem
+        fm.downloadFileFromiCloud(jsonPath);
+
+        const loadedJSON = JSON.parse(fm.readString(jsonPath));
+        if (loadedJSON !== null) {
+            return loadedJSON;
+        } else {
+            throw ("Could not read file as JSON! Could not load: " + jsonPath);
+        }
+    }
+
+    getFileManager() {
+        try {
+            return FileManager.iCloud();
+        } catch (e) {
+            return FileManager.local();
+        }
+    }
+
+    getCurrentDir() {
+        const fm = this.getFileManager();
+        const thisScriptPath = module.filename;
+        return thisScriptPath.replace(fm.fileName(thisScriptPath, true), '');
+    }
+
+}
+
+const jsonFileManager = new JSONFileManager();
 const params = { "apiKey": "testtest" };
 
-function getFileManager() {
-    try {
-        return FileManager.iCloud();
-    } catch (e) {
-        return FileManager.local();
-    }
-}
-
-function getCurrentDir() {
-    const fm = getFileManager();
-    const thisScriptPath = module.filename;
-    return thisScriptPath.replace(fm.fileName(thisScriptPath, true), '');
-}
-
-/**
- * Attempts to load parameters stored in the file ./storage/name.json
- * Returns null if it cannot be loaded.
- */
-function loadStoredParameters(name) {
-    const fm = getFileManager();
-    const storageDir = getCurrentDir() + "storage";
-    const parameterPath = storageDir + "/" + name + ".json";
-
-    if (!fm.fileExists(storageDir)) {
-        console.log("Storage folder does not exist!");
-        return null;
-    } else if (!fm.isDirectory(storageDir)) {
-        console.log("Storage folder exists but is not a directory!");
-        return null;
-    } else if (!fm.fileExists(parameterPath)) {
-        console.log("Parameter file does not exist!");
-        return null;
-    } else if (fm.isDirectory(parameterPath)) {
-        console.log("Parameter file is a directory!");
-        return null;
-    }
-
-    // Doesn't fail with local filesystem
-    fm.downloadFileFromiCloud(parameterPath);
-
-    const parameterJSON = JSON.parse(fm.readString(parameterPath));
-    if (parameterJSON !== null) {
-        return parameterJSON;
-    } else {
-        console.log("Could not load parameter file as JSON!");
-        return null;
-    }
-}
-
-/**
- * Attempts to write parameters to the file ./storage/name.json
- */
-function writeStoredParameters(name, params) {
-    const fm = getFileManager();
-    const storageDir = getCurrentDir() + "storage";
-    const parameterPath = storageDir + "/" + name + ".json";
-
-    if (!fm.fileExists(storageDir)) {
-        console.log("Storage folder does not exist! Creating now.");
-        fm.createDirectory(storageDir);
-    } else if (!fm.isDirectory(storageDir)) {
-        throw ("Storage folder exists but is not a directory!");
-    }
-
-    if (fm.fileExists(parameterPath) && fm.isDirectory(parameterPath)) {
-        throw ("Parameter file is a directory, please delete!");
-    }
-
-    fm.writeString(parameterPath, JSON.stringify(params));
-}
-
-console.log(loadStoredParameters("test"));
-writeStoredParameters("test", params);
-console.log(loadStoredParameters("test"));
+console.log(jsonFileManager.read("test"));
+jsonFileManager.write("test", params);
+console.log(jsonFileManager.read("test"));
